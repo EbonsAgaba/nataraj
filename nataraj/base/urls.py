d2d01852d9f8dbdf0743a3d1b6234feae3bfe5c1
Ebons <eboagi@live.com>
2024-02-25 22:11:07 +0300

    adding update and delete functionalities for room

--- nataraj/base/urls.py
+++ nataraj/base/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name="room"),
 
     path('create_room/', views.createRoom, name="create-room"),
+    path('update_room/<str:pk>', views.updateRoom, name="update-room"),
+    path('delete_room/<str:pk>', views.deleteRoom, name="delete-room"),
 
 ]
